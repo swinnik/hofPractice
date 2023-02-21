42e5016a7205f87245b2d6a38a2878add16a1193
Sean Winnik <winnik.sean@gmail.com>
2023-02-20 17:52:49 -0800

    passing all of practice.js

--- practice.js
+++ practice.js
@@ -71,10 +71,10 @@ var startsWith = function (fruits, letter) {
 
 // return a filtered array containing only cookie-type desserts.
 var cookiesOnly = function (desserts) {
-  desserts = _.filter(desserts, function(dessert) {
+  var gF = _.filter(desserts, function(dessert) {
     return dessert.type === 'cookie';
   });
-  return desserts;
+  return gF;
 };
 
 // rebuild the getUserTweets function from above with _.filter instead
@@ -104,22 +104,24 @@ var upperCaseFruits = function (fruits) {
 // that have a new "glutenFree" property, with a boolean value.
 // TIP: Items that contain flour are not gluten-free.
 var glutenFree = function (desserts) {
-  var gF = {...desserts}
-  _.map(gF, function(dessert) {
+  _.map(desserts, function(dessert) {
     !dessert.ingredients.includes('flour') ?
     dessert.glutenFree = true : dessert.glutenFree = false;
   });
-  return gF;
+  return desserts;
 };
 
 
 // given an array of tweet objects, return a new array of strings
 // containing only the message properties.
 var allUserMessages = function(tweets) {
-
+  var messages = _.map(tweets, function(tweet) {
+    return tweet.message;
+  });
+  return messages;
 };
 
-// use _.map to return an array of items with their sale prices, with a new property
+// use _.map to return an array of items with their prices, with a new property
 // containing the sale price. round any decimals to 2 places.
 //
 // having trouble with decimals? check out this article:
@@ -141,6 +143,15 @@ var allUserMessages = function(tweets) {
 */
 var applyCoupon = function (groceries, coupon) {
 
+  var saleIncluded = _.map(groceries, function(item) {
+    var currency = item.price.replace(/[$,]+/g, '' );
+    var salePrice = currency * 0.8;
+    item.salePrice = '$' + salePrice.toFixed(2);
+
+  } );
+
+
+  return groceries;
 };
 
 /*
@@ -151,6 +162,12 @@ var applyCoupon = function (groceries, coupon) {
 
 // return the total price of all products.
 var sumTotal = function (products) {
+  var total = 0;
+  total = _.reduce(products, function(memo, item) {
+    var number = item.price.replace(/[$,]+/g, '' );
+    return number * 1 + memo;
+  }, 0 );
+  return total;
 
 };
 
@@ -158,6 +175,22 @@ var sumTotal = function (products) {
 // exampleOutput: { dessertType: 3, dessertType2: 1 }
 var dessertCategories = function (desserts) {
 
+  _.reduce(desserts, function(memo, desserts) {
+    return memo;
+  });
+
+  var dTypes = {};
+  _.each(desserts, function(item) {
+    if (!dTypes.hasOwnProperty(item.type)) {
+      dTypes[item.type] = 1;
+    } else {
+      dTypes[item.type] = dTypes[item.type] + 1;
+    }
+
+  });
+
+
+  return dTypes;
 };
 
 // return an object with the proper count of all user messages
@@ -172,19 +205,49 @@ var dessertCategories = function (desserts) {
   }
 */
 var countMessagesPerUser = function(tweets) {
+  var userCount = {};
 
+  _.reduce(tweets, function(tweet) {});
+
+  _.each(tweets, function(tweet) {
+    if (!userCount.hasOwnProperty(tweet.user)) {
+      userCount[tweet.user] = 1;
+    } else {
+      userCount[tweet.user] = userCount[tweet.user] + 1;
+    }
+  });
+  return userCount;
 };
 
 // given an array of movie data objects,return an array containing
 // movies that came out between 1990 and 2000.
 // TIP: use an array as your accumulator - don't push to an external array!
 var ninetiesKid = function (movies) {
+  var nineties = _.reduce(movies, function(memo, film) {
 
+    if (film.releaseYear < 2000 && film.releaseYear >= 1990) {
+      memo.push(film.title);
+      return memo;
+    } return memo;
+
+  }, []);
+  return nineties;
 };
 
 // return an boolean stating if there exists a movie with a shorter
 // runtime than your time limit.
 // timeLimit is an integer representing a number of minutes.
 var movieNight = function (movies, timeLimit) {
+
+
+  var letsWatch = _.reduce(movies, function(memo, film) {
+    if (film.runtime <= timeLimit) {
+      memo = true;
+      return memo;
+    } return memo;
+  }, false);
+
+
+  return letsWatch;
 
 };
